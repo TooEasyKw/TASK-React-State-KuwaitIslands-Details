f2b9fe5b96499752f6c8ded569bac9455243ff2c
Dawood <dawood.boland.kw@gmail.com>
2024-05-16 02:46:31 +0300

    All Taskssssssssssssssssssssssssssss Done

--- src/Components/Island.js
+++ src/Components/Island.js
@@ -1,13 +1,17 @@
 import React, { useEffect } from "react";
 
-export default function Island({ island, setIsland }) {
+export default function Island({
+  island,
+  setIsland,
+  islandsList,
+  updateIsland,
+}) {
   const petIsland = () => {
     setIsland(island);
+    updateIsland(island.id);
   };
   useEffect(() => {
-    console.log(
-      "Count for " + island.name + ": " + island.visitors
-    );
+    console.log("Count for " + island.name + ": " + island.visitors);
   }, [island.visitors]);
   return (
     <div className="Island" onClick={petIsland}>
